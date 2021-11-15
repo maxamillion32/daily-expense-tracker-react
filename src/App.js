8e820fdd42d05c41870bc01fad0c15ab7c84fd1a
Stanislaw <post.chirko@gmail.com>
2021-11-15 23:00:21 +0100

    fix: add navigate for not exist route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, Routes} from 'react-router-dom';
+import {Route, Routes, Navigate} from 'react-router-dom';
 import Layout from '../src/hoc/Layout/Layout'
 
 import Transactions from './containers/Transactions/Transactions';
@@ -13,7 +13,7 @@ function App() {
           <Route path="/" element={<Transactions />} />
           <Route path="/budget" element={<Budget />} />
           <Route path="/settings" element={<Settings />} />
-          <Route path="*" element={<Transactions />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Layout>
     );
