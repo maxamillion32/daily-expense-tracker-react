62913d15bd7fb279835582e861da133c26a2209d
Stanislaw <post.chirko@gmail.com>
2021-12-06 21:51:26 +0100

    chore: change the display of the expenses graph in the annual report

--- src/components/Widgets/YearOutcomes/YearOutcomes.js
+++ src/components/Widgets/YearOutcomes/YearOutcomes.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import classes from './YearOutcomes.module.css';
-import {updateMonth} from '../../../reducers/transactions/transactions-slice'
+import {formatMonth} from '../../../utils/utils';
+
+import {
+  updateMonth,
+  selectAllTransactionsState
+} from '../../../reducers/transactions/transactions-slice'
 
 function YearOutcomes({currentMonth}) {
+  const allTransactions = useSelector(selectAllTransactionsState);
   const dispatch = useDispatch();
+
+  const getOverallPercent = (month) => {
+    const outcome = allTransactions
+      .filter((transaction) => formatMonth(transaction.date) === month)
+      .map((transaction) => transaction.outcome ? transaction = +transaction.sum : transaction = null)
+      .reduce((acc, sum) => acc + sum, 0);
+
+    let overallPercent = outcome / 1000 * 100;
+
+    return overallPercent === 0 ? 1 : overallPercent;
+  }
 
   const monthOutcomes = [
     'January', 'February', 'March',
@@ -25,9 +42,10 @@ function YearOutcomes({currentMonth}) {
             <div
               className={`${classes.List} ${month === currentMonth ? classes.Active : ''}`}
               key={month}
+              id={month}
               onClick={monthHandler}
             >
-              <div id={month}></div>
+              <div id={month} style={{height: `${getOverallPercent(month)}%`}}></div>
               <p id={month}>{month.slice(0, 3)}</p>
             </div>
             ))
