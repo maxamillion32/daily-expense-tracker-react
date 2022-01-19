fe0386d5fd29f3c5075c7a6ec2d30ad757f5d8f6
Stanislaw <post.chirko@gmail.com>
2022-01-19 11:04:15 +0100

    chore: add datalist for search component

--- src/modules/transactions/components/Search/Search.js
+++ src/modules/transactions/components/Search/Search.js
@@ -1,10 +1,13 @@
 import React, {useSelector, useDispatch} from "react-redux";
 import {setSearchTerm, selectSearchTerm, clearSearchTerm} from "../../../../reducers/search/search-slice";
+import {selectAllCategoriesState} from "../../../../reducers/categories/categories-slice";
 import CloseButton from "../../../common/components/CloseButton/CloseButton";
 import classes from "./Search.module.css";
 
 function Search() {
   const searchTerm = useSelector(selectSearchTerm);
+  const getCategories = useSelector(selectAllCategoriesState);
+  const categories = [...getCategories];
   const dispatch = useDispatch();
 
   const onSearchTermChangeHandler = (e) => {
@@ -24,10 +27,17 @@ function Search() {
         placeholder="Search by category"
         value={searchTerm}
         onChange={onSearchTermChangeHandler}
+        list="categories"
+        autoComplete="off"
       />
+      <datalist id="categories">
+        {categories.map((category) =>
+          <option key={category.id} value={category.title} />
+        )}
+      </datalist>
       {searchTerm.length > 0
         ? <div className={classes.CloseBtn}>
-            <CloseButton onClick={onClearSearchTermHandler}/>
+            <CloseButton onClick={onClearSearchTermHandler} />
           </div>
         : null}
     </section>
