87660c9a0fa9da4a67f3a3cb87ead7d04a84cc8f
Stanislaw <post.chirko@gmail.com>
2022-01-19 10:32:56 +0100

    chore: hide search component by transactions length = 0

--- src/modules/transactions/components/Container.js
+++ src/modules/transactions/components/Container.js
@@ -5,7 +5,7 @@ import classes from "./Container.module.css";
 
 import {
   loadTransactions,
-  // selectAllTransactionsState,
+  selectAllTransactionsState,
   showButton, isLoading
 } from "../../../reducers/transactions/transactions-slice";
 import {
@@ -21,9 +21,11 @@ import Welcome from "../../welcome/components/Welcome";
 function TransactionsContainer() {
   const loading = useSelector(isLoading);
   const userId = useSelector(selectUserId);
+  const transactions = useSelector(selectAllTransactionsState);
   const dispatch = useDispatch();
 
   const isLoader = loading && userId;
+  const isTransactions = transactions.length !== 0;
 
   useEffect(() => {
     dispatch(showButton());
@@ -42,7 +44,7 @@ function TransactionsContainer() {
       {!isLoader && userId
         ? <section className={classes.Container}>
             <Balance />
-            <Search />
+            {isTransactions ? <Search /> : null}
             <TransactionsListContainer />
           </section>
         : null}
