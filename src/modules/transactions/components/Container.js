9c978aad6d118cfcdb470640321d80b7e5298b2b
Stanislaw <post.chirko@gmail.com>
2022-01-20 08:39:49 +0100

    fix: wheel events

--- src/modules/transactions/components/Container.js
+++ src/modules/transactions/components/Container.js
@@ -6,7 +6,7 @@ import classes from "./Container.module.css";
 import {
   loadTransactions,
   selectAllTransactionsState,
-  showButton, isLoading, isButtonClick
+  showButton, isLoading
 } from "../../../reducers/transactions/transactions-slice";
 import {
   selectUserId
@@ -22,82 +22,18 @@ function TransactionsContainer() {
   const loading = useSelector(isLoading);
   const userId = useSelector(selectUserId);
   const transactions = useSelector(selectAllTransactionsState);
-  const clickAddButton = useSelector(isButtonClick);
   const dispatch = useDispatch();
 
   const isLoader = loading && userId;
   const isTransactions = transactions.length !== 0;
 
-  const handleNavigation = (event) => {
-      if (event.deltaY > 0) {
-          dispatch(showButton(false));
-      } else if (event.deltaY < 0) {
-          dispatch(showButton(true));
-      }
-  };
-
-  let xDown = null;
-  let yDown = null;
-
-  function getTouches(evt) {
-    return evt.touches || evt.originalEvent.touches;
-  }
-
-  function handleTouchStart(evt) {
-      const firstTouch = getTouches(evt)[0];
-      xDown = firstTouch.clientX;
-      yDown = firstTouch.clientY;
-  }
-
-  function handleTouchMove(evt) {
-      if ( ! xDown || ! yDown ) {
-          return;
-      }
-
-      const xUp = evt.touches[0].clientX;
-      const yUp = evt.touches[0].clientY;
-
-      const xDiff = xDown - xUp;
-      const yDiff = yDown - yUp;
-
-      if (Math.abs(xDiff) > Math.abs(yDiff)) {
-          if ( xDiff > 0 ) {
-               //left swipe
-          } else {
-              //right swipe
-          }
-      } else {
-          if ( yDiff > 0 ) {
-            dispatch(showButton(false)); //up swipe
-          } else {
-            dispatch(showButton(true)); //down swipe
-          }
-      }
-      //reset values
-      xDown = null;
-      yDown = null;
-  }
-
   useEffect(() => {
     dispatch(showButton(true));
     dispatch(loadTransactions(userId));
-
-    document.addEventListener("wheel", handleNavigation, false);
-    document.addEventListener("touchstart", handleTouchStart, false);
-    document.addEventListener("touchmove", handleTouchMove, false);
-
-    if (clickAddButton) {
-      document.removeEventListener("wheel", handleNavigation, false);
-      document.removeEventListener("touchstart", handleTouchStart, false);
-      document.removeEventListener("touchmove", handleTouchMove, false);
-    }
     return () => {
       dispatch(showButton(false));
-      document.removeEventListener("wheel", handleNavigation, false);
-      document.removeEventListener("touchstart", handleTouchStart, false);
-      document.removeEventListener("touchmove", handleTouchMove, false);
     };
-  }, [userId, clickAddButton]);
+  }, [userId]);
 
   return (
     <>
--- src/modules/transactions/components/List/Container.js
+++ src/modules/transactions/components/List/Container.js
@@ -1,7 +1,7 @@
-import React from "react";
-import {useSelector} from "react-redux";
+import React, {useEffect} from "react";
+import {useSelector, useDispatch} from "react-redux";
 
-import {selectFilteredTransactions} from "../../../../reducers/transactions/transactions-slice";
+import {selectFilteredTransactions, isButtonClick, showButton} from "../../../../reducers/transactions/transactions-slice";
 import {selectAllCategoriesState} from "../../../../reducers/categories/categories-slice";
 import {selectAllAccountsState} from "../../../../reducers/accounts/accounts-slice";
 
@@ -12,14 +12,83 @@ import classes from "./Container.module.css";
 function TransactionsListContainer() {
   const getCategories = useSelector(selectAllCategoriesState);
   const getAccounts = useSelector(selectAllAccountsState);
+  const clickAddButton = useSelector(isButtonClick);
   const categories = [...getCategories];
   const accounts = [...getAccounts];
+  const dispatch = useDispatch();
 
   const isEmpty = categories.length === 0 || accounts.length === 0;
 
   const transactions = useSelector(selectFilteredTransactions);
   const months = [...new Set(transactions
     .map(date => formatMonth(date.date)))];
+
+  const handleNavigation = (event) => {
+      if (event.deltaY > 0) {
+          dispatch(showButton(false));
+      } else if (event.deltaY < 0) {
+          dispatch(showButton(true));
+      }
+  };
+
+  let xDown = null;
+  let yDown = null;
+
+  function getTouches(evt) {
+    return evt.touches || evt.originalEvent.touches;
+  }
+
+  function handleTouchStart(evt) {
+      const firstTouch = getTouches(evt)[0];
+      xDown = firstTouch.clientX;
+      yDown = firstTouch.clientY;
+  }
+
+  function handleTouchMove(evt) {
+      if ( ! xDown || ! yDown ) {
+          return;
+      }
+
+      const xUp = evt.touches[0].clientX;
+      const yUp = evt.touches[0].clientY;
+
+      const xDiff = xDown - xUp;
+      const yDiff = yDown - yUp;
+
+      if (Math.abs(xDiff) > Math.abs(yDiff)) {
+          if ( xDiff > 0 ) {
+               //left swipe
+          } else {
+              //right swipe
+          }
+      } else {
+          if ( yDiff > 0 ) {
+            dispatch(showButton(false)); //up swipe
+          } else {
+            dispatch(showButton(true)); //down swipe
+          }
+      }
+      //reset values
+      xDown = null;
+      yDown = null;
+  }
+
+  useEffect(() => {
+    document.addEventListener("wheel", handleNavigation, false);
+    document.addEventListener("touchstart", handleTouchStart, false);
+    document.addEventListener("touchmove", handleTouchMove, false);
+
+    if (clickAddButton) {
+      document.removeEventListener("wheel", handleNavigation, false);
+      document.removeEventListener("touchstart", handleTouchStart, false);
+      document.removeEventListener("touchmove", handleTouchMove, false);
+    }
+    return () => {
+      document.removeEventListener("wheel", handleNavigation, false);
+      document.removeEventListener("touchstart", handleTouchStart, false);
+      document.removeEventListener("touchmove", handleTouchMove, false);
+    };
+  }, [clickAddButton]);
 
   return (
     <section className="transactions">
