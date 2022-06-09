f55291d8c944ff06ad4b1808ee47efe1a907a5ae
Stanislaw <post.chirko@gmail.com>
2022-06-09 22:18:01 +0200

    feat: add dynamic pagination for transactions

--- src/modules/transactions/components/List/Items/YearGroup.js
+++ src/modules/transactions/components/List/Items/YearGroup.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {useSelector} from "react-redux";
 
 import classes from "../Container.module.css";
@@ -12,13 +12,49 @@ function TransactionsItemsYearGroup({year}) {
   const getFilteredTransactions = useSelector(selectFilteredTransactions);
   const filteredTransactions = [...getFilteredTransactions];
 
-  const yearTransactions = filteredTransactions
-    .filter((transaction) => formatYear(transaction.date) === year);
+  const [transaction, setTransactions] = useState([]);
+  const [currentPage, setCurrentPage] = useState(10);
+  const [prevPage, setPrevPage] = useState(0);
+  const [fetching, setFetching] = useState(true);
+  const [totalCount, setTotalCount] = useState(0);
+
+  useEffect(() => {
+    if (fetching) {
+      setTransactions([...transaction, ...filteredTransactions.slice(prevPage, currentPage)]);
+      setCurrentPage((prevState) => prevState + 10);
+      setPrevPage((prevState) => prevState + 10);
+      setTotalCount(transaction.length);
+      setFetching(false);
+    }
+  }, [fetching]);
+
+  const yearTransactions = transaction
+  .filter((transaction) => formatYear(transaction.date) === year);
   const nodeRef = React.useRef(null);
   const months = [...new Set(yearTransactions
     .map(date => formatMonth(date.date)))];
 
   const isCurrentYear = getCurrentYear === year;
+
+  useEffect(() => {
+    document.addEventListener("scroll", scrollHandler);
+
+    return function () {
+      document.removeEventListener("scroll", scrollHandler);
+    };
+  }, []);
+
+  const scrollHandler = (e) => {
+    const scrollHeight = e.target.documentElement.scrollHeight;
+    const scrollTop = e.target.documentElement.scrollTop;
+    const innerHeight = window.innerHeight;
+
+    if (scrollHeight - (scrollTop + innerHeight) < 100 && totalCount < filteredTransactions.length) {
+      setFetching(true);
+    } else {
+      setFetching(false);
+    }
+  };
 
   return (
     <ul className="transactions__list">
