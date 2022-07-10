19786bd74076803b09c7934a0038c6b78f669b4a
stanislaw-ch <post.chirko@gmail.com>
2022-07-10 23:27:03 +0200

    fix: fix import store

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import {createRoot} from "react-dom/client";
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import App from "./App";
-import store from "./store/store";
+import {store} from "./store/store";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
