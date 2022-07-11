d6eaadeffc7b67cf883fe75ab2ef637e11313d53
stanislaw-ch <post.chirko@gmail.com>
2022-07-11 18:00:25 +0200

    refactor: change js to typescript in navigation-slice

---
+++ src/reducers/navigation/navigation-slice.tsx
@@ -0,0 +1,50 @@
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {RootState} from "../../store/store";
+
+interface NavigationState {
+  isAddButtonClick: boolean,
+  isButtonShow: boolean,
+  IsTransactionTypeClick: boolean,
+}
+
+const initialState: NavigationState = {
+  isAddButtonClick: false,
+  isButtonShow: false,
+  IsTransactionTypeClick: false,
+};
+
+export const navigationSlice = createSlice({
+  name: "navigation",
+  initialState,
+  reducers: {
+    setIsButtonShow: (state, action: PayloadAction<boolean>) => {
+      return {
+        ...state,
+        isButtonShow: action.payload === "" ? !state.isButtonShow : action.payload,
+      };
+    },
+    setIsAddButtonClick: (state, action: PayloadAction<boolean>) => {
+      return {
+        ...state,
+        isAddButtonClick: action.payload === "" ? !state.isAddButtonClick : action.payload,
+      };
+    },
+    setIsTransactionTypeClick: (state) => {
+      return {
+        ...state,
+        IsTransactionTypeClick: !state.IsTransactionTypeClick,
+      };
+    },
+  },
+});
+
+export const selectIsAddButtonClick = (state: RootState) => state.navigation.isAddButtonClick;
+export const selectIsButtonShow = (state: RootState) => state.navigation.isButtonShow;
+export const selectIsTransactionTypeClick = (state: RootState) => state.navigation.IsTransactionTypeClick;
+
+export const {
+  setIsAddButtonClick,
+  setIsButtonShow,
+  setIsTransactionTypeClick
+} = navigationSlice.actions;
+export default navigationSlice.reducer;
