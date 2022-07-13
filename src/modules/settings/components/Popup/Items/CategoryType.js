8bd9daafb307a531da3f1def2f4c549cf331fdae
stanislaw-ch <post.chirko@gmail.com>
2022-07-13 20:02:54 +0200

    refactor: add React.memo for CategoryType component

--- src/modules/settings/components/Popup/Items/CategoryType.js
+++ src/modules/settings/components/Popup/Items/CategoryType.js
@@ -1,20 +1,30 @@
-import React from "react";
+import React, {memo, useMemo} from "react";
 import Input from "../../../../common/components/Input/Input";
 import {isExists} from "../utils/utils";
 
-export const CategoryType = ({transactions, prevItemState, incomes, onChange, classes}) => (
-  <div className={classes.Type}>
-    {isExists(transactions, "category", prevItemState.title)
-      ? <p className={classes.Label}>This category is already used in transactions and this setting cannot be changed.</p>
-      : <p className={classes.Label}>Select <b>incomes</b> if the category is taken into income transactions</p>}
-    <Input
-      type="checkbox"
-      name="type-of-category"
-      label={"Incomes"}
-      placeholder="0.00"
-      checked={+incomes || false}
-      onChange={onChange}
-      isDisabled={isExists(transactions, "category", prevItemState.title)}
-    />
-  </div>
-);
+function CategoryType({transactions, prevItemState, incomes, onChange, classes}) {
+  return (
+    <div className={classes.Type}>
+      {useMemo(() => isExists(transactions, "category", prevItemState.title), [prevItemState.title])
+        ? <p className={classes.Label}>This category is already used in transactions and this setting cannot be
+          changed.</p>
+        : <p className={classes.Label}>Select <b>incomes</b> if the category is taken into income transactions</p>}
+      <Input
+        type="checkbox"
+        name="type-of-category"
+        label={"Incomes"}
+        placeholder="0.00"
+        checked={+incomes || false}
+        onChange={onChange}
+        isDisabled={useMemo(() => isExists(transactions, "category", prevItemState.title), [prevItemState.title])}
+        errorMessage={""}
+        shouldValidate={false}
+        touched={false}
+        valid={true}
+        value={null}
+      />
+    </div>
+  );
+}
+
+export default memo(CategoryType);
