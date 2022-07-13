bc6641e62a228fcc8c3c3b8fb00fd2012d153d9a
stanislaw-ch <post.chirko@gmail.com>
2022-07-13 20:27:58 +0200

    refactor: change export for StartBalance, CurrentBalance, ChangeBalance, DateOfBalance

--- src/modules/settings/components/Popup/Items/index.js
+++ src/modules/settings/components/Popup/Items/index.js
@@ -2,10 +2,10 @@ import {Buttons} from "./Buttons";
 import {Title} from "./Title";
 import CategoryType from "./CategoryType";
 import CategoryExpenses from "./CategoryExpenses";
-import {StartBalance} from "./StartBalance";
-import {CurrentBalance} from "./CurrentBalance";
-import {ChangeBalance} from "./ChangeBalance";
-import {DateOfBalance} from "./DateOfBalance";
+import StartBalance from "./StartBalance";
+import CurrentBalance from "./CurrentBalance";
+import ChangeBalance from "./ChangeBalance";
+import DateOfBalance from "./DateOfBalance";
 
 export {
   Buttons,
