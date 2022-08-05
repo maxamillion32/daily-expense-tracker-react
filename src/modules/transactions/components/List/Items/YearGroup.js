dccd92ba93277459a485425270643305cba7118a
stanislaw-ch <post.chirko@gmail.com>
2022-08-05 15:05:45 +0200

    fix: fix transaction lazy loading by search

--- src/modules/transactions/components/List/Items/YearGroup.js
+++ src/modules/transactions/components/List/Items/YearGroup.js
@@ -3,7 +3,7 @@ import {useSelector} from "react-redux";
 
 import classes from "../Container.module.css";
 import TransactionsItemsMonthGroup from "./MonthGroup";
-import {formatYear, formatMonth, usePrevious, isEqual} from "../../../../common/utils/utils";
+import {formatYear, formatMonth, usePrevious} from "../../../../common/utils/utils";
 
 import {selectCurrentYear, selectFilteredTransactions} from "../../../../../reducers/transactions/transactions-slice";
 import {selectSearchTerm} from "../../../../../reducers/search/search-slice";
@@ -23,25 +23,16 @@ function TransactionsItemsYearGroup({year}) {
 
   const [transaction, setTransactions] = useState([]);
   const [currentPage, setCurrentPage] = useState(PAGINATION.INITIAL_AMOUNT);
-  const [prevPage, setPrevPage] = useState(0);
-  const [fetching, setFetching] = useState(true);
   const [totalCount, setTotalCount] = useState(0);
 
   useEffect(() => {
-    if (fetching) {
-      setTransactions([...transaction, ...filteredTransactions.slice(prevPage, currentPage)]);
-      setCurrentPage((prevState) => prevState + PAGINATION.SHIFT);
-      setPrevPage((prevState) => prevState + PAGINATION.SHIFT);
-      setTotalCount(transaction.length);
-      setFetching(false);
+    if (searchTerm === "" && prevSearchTerm) {
+      setCurrentPage(PAGINATION.INITIAL_AMOUNT);
     }
 
-    if (isEqual(searchTerm, prevSearchTerm)) {
-      setTransactions(filteredTransactions);
-      setTotalCount(transaction.length);
-      setFetching(true);
-    }
-  }, [fetching, searchTerm]);
+    setTransactions(filteredTransactions.slice(0, currentPage));
+    setTotalCount(transaction.length);
+  }, [currentPage, searchTerm, prevSearchTerm]);
 
   const yearTransactions = transaction.filter((transaction) => formatYear(transaction.date) === year);
   const nodeRef = React.useRef(null);
@@ -64,9 +55,7 @@ function TransactionsItemsYearGroup({year}) {
     const innerHeight = window.innerHeight;
 
     if (scrollHeight - (scrollTop + innerHeight) < 100 && totalCount < filteredTransactions.length) {
-      setFetching(true);
-    } else {
-      setFetching(false);
+      setCurrentPage((prevState) => prevState + PAGINATION.SHIFT);
     }
   };
 
